8fb9e894e90e029dad4cf933263c090693794b47
ayunas <ayunas@gmail.com>
2019-05-21 13:09:10 -0500

    authentication react app basic setup

--- authentication/src/App.js
+++ authentication/src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Login from "./Login";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      url: "http://localhost:5660/api"
+    };
+  }
+
+  login = (username, password) => {
+    console.log(
+      "%c login has been triggered!",
+      "font-size:16px; color: purple;"
+    );
+    const credentials = { username, password };
+    axios
+      .post(`${this.state.url}/login`, credentials)
+      .then(res => {
+        console.log(
+          `%c response data: ${res.body}`,
+          "font-size:16px; color:orange;"
+        );
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  };
+
+  register = (username, password) => {
+    console.log(
+      "%c register has been triggered!",
+      "font-size:16px; color: blue;"
+    );
+    const newUser = { username, password };
+
+    axios
+      .post(`${this.state.url}/register`, newUser)
+      .then(res => {
+        console.log(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <header>Welcome to Authenticator</header>
+          <Login login={this.login} register={this.register} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
