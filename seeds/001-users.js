4d08f0a34f42177c074fe7e5a10853cae420cc72
ayunas <ayunas@gmail.com>
2019-05-20 23:18:45 -0500

    added react-app, finished endpoints and stretch endpoint

--- seeds/001-users.js
+++ seeds/001-users.js
@@ -1,13 +1,24 @@
+const bcrypt = require("bcryptjs");
+const hashedPassword1 = bcrypt.hashSync('password', 10);
+const hashedPassword2 = 
+const hashedPassword3 = 
+
+
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
+
+
   return knex("users")
     .truncate()
     .then(function() {
       // Inserts seed entries
       return knex("users").insert([
-        { username: "amir", password: "password" },
+        { username: "amir", password: bcrypt.hashSync('password', 10) },
         { username: "sheena", password: "password" },
         { username: "sofia", password: "password" }
       ]);
     });
 };
+
+
+//
