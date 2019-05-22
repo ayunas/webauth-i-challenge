f2a028b6ec74393d17d850d185eecc8be703898f
ayunas <ayunas@gmail.com>
2019-05-22 15:56:15 -0500

    added a KnexSessionStore, connected to the SQLite3 database and created a sessions table that stores the session data and cookies

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 const router = require("./router");
-const session = require("express-session"); //sets up session and creates cookie
+const session = require("express-session");
+//sets up session and creates cookie
+const KnexSessionStore = require("connect-session-knex")(session); //this returns a constructor function which by convention is uppercased
 // const SessionStore = require("connect-session-knex")(session); //to store in the database.
 // const SessionStore = require("connect-dynamodb")(session);
 // var SQLiteStore = require('connect-sqlite3')(express);
@@ -11,13 +13,13 @@ const bodyParser = express.json();
 
 const server = express();
 
-// const sessionTable = {
-//   knex: require("./data/dbConfig"),
-//   tablename: "sessions",
-//   sidfieldname: "sid",
-//   createtable: "true",
-//   clearInterval: 1000 * 60 * 60
-// };
+const storeConfig = {
+  knex: require("./data/dbConfig"),
+  tablename: "sessions", //optional
+  sidfieldname: "sid", //optional
+  createtable: "true",
+  clearInterval: 1000 * 60 * 60 //clear out expired sessions
+};
 
 const sessionConfig = {
   //this is for the express-session
@@ -29,8 +31,9 @@ const sessionConfig = {
     httpOnly: true //cannot manipulate the cookie by using JS
   },
   resave: false, //if set to true, session will be recreated, even those tht have not changed.
-  saveUninitialized: false // prevents from automatically setting cookies without permission from user
+  saveUninitialized: false, // prevents from automatically setting cookies without permission from user
   // store: new SessionStore(sessionTable)
+  store: new KnexSessionStore(storeConfig)
 };
 
 server.use(bodyParser, cors(), session(sessionConfig));
